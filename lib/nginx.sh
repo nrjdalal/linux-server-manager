62233e53e62b760ec6eb174e67f63990642e3f6a
Neeraj Dalal <admin@nrjdalal.com>
2022-01-21 17:56:47 +0530

    1.0.0

--- lib/nginx.sh
+++ lib/nginx.sh
@@ -1,7 +1,8 @@
 url="$LIB_URL/nginx"
 
 case ${data[1]} in
-proxy | proxy-80 | proxy-443) src "$url/proxy-443.sh" ;;
+proxy-80) src "$url/proxy-80.sh" ;;
+proxy | proxy-443) src "$url/proxy-443.sh" ;;
 root-80) src "$url/root-80.sh" ;;
 root-443) src "$url/root-443.sh" ;;
 esac
